ae298606bea4e3741f19fcc0ccbbd643534aea6d
Zach Stone <zachinjapan888s@gmail.com>
2021-07-10 12:00:54 +0900

    worked more on drum se
    used the this function and started to work on figuring out how to the make the text white when clicked.

--- Javascript/The Drum Kit/index.js
+++ Javascript/The Drum Kit/index.js
@@ -2,9 +2,23 @@ var number_of_drum_buttons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < (number_of_drum_buttons); i++) {
 
-    document.querySelectorAll("button")[i].addEventListener("click", clickEvent);
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
 
-    function clickEvent() {
-        alert("I was clicked");
-    }
+        console.log(this);
+        document.querySelector(this).addAttribute(text)
+
+    });
+
 }
+
+
+
+
+
+
+
+
+// function clickEvent() {
+//     var tom_1 = new Audio("sounds/tom-1.mp3");
+//     tom_1.play();
+// }
